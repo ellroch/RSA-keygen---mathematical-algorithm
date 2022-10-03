3a4e7e00dbdb29bc32d9ef068bc1fa17882a0329
OrcGorp <glanzer.christopher@gmail.com>
2022-10-02 17:24:42 -0700

    upload of completed files

---
+++ intro to discrete-coprimes and rsa/testing area/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+string= 'words'
+
+for index, letter in enumerate(string):
+    print((letter, index))
+
+int x= npl.linespace(0, 10, 500)
+
+x= 2 * x
